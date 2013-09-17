3f1f2d0a53f517c759756805da09e18cab2ce900
Yoo, Derrick <dky@email.arizona.edu>
2013-09-17 11:33:34 -0700

    remove Work

--- Work/specialhandling/specialhandling/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from specialhandling.models import CheckInfo
-
-class SpecialHandlingForm(ModelForm):
-    class Meta:
-        model = CheckInfo
-        exclude = ('net_id', 'created_at', 'updated_at', 'picked_up')
